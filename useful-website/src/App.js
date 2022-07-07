f2ba14b385d42ad832334a96559ee87d442a09e9
lalit saud <lalitsaud1997@gmail.com>
2022-07-07 10:20:54 +0545

    useful website for programmer

---
+++ useful-website/src/App.js
@@ -0,0 +1,20 @@
+import Button from "./Button";
+
+const App = ()=> {
+    return <div>
+        <h1>BEST FRIENDS OF EVERY</h1>
+        <h2>SELF-TAUGHT PROGRAMMER</h2>
+        <Button name="Google" link="https://www.google.com/"/>
+        <Button name="YouTube" link="https://www.youtube.com/"/>
+        <Button name="StackOverflow" link="https://stackoverflow.com/"/>
+        <Button name="GitHub" link="https://github.com/"/>
+        <br/>
+        <Button name="W3School" link="https://www.w3schools.com/"/>
+        <Button name="GreeksforGreeeks" link="https://www.geeksforgeeks.org/"/>
+        <Button name="Udemy" link="https://www.udemy.com/"/>
+        <Button name="Hacker Rank" link="https://www.hackerrank.com/"/>
+        
+    </div>
+}
+
+export default App;
---
+++ useful-website/src/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Button = (props) => {
+    return <a href={props.link} target="_blank"><button className="button">{props.name}</button></a>
+}
+
+export default Button;
